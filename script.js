1b215832055b253923563ca8dbf79a793ea0a1d9
Ruhul Amin <ruhulamin3052@gmail.com>
2025-08-23 01:34:07 +0600

    added all section

---
+++ script.js
@@ -0,0 +1,6 @@
+// Logging button funclity
+document.getElementById('loggingButton')
+    .addEventListener('click', function(e){
+        e.preventDefault()
+        console.log('logging button connect');
+    })
