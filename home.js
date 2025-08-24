ef57e2a911c033296d1a6c86dffa2200997e4da5
Ruhul Amin <ruhulamin3052@gmail.com>
2025-08-25 01:13:02 +0600

    added all section

--- home.js
+++ home.js
@@ -1,1 +1,21 @@
-console.log('home file connected')
+
+    const validPinNumber = 1234
+    document.getElementById('add-money-btn')
+        .addEventListener('click', function(e){
+            e.preventDefault();
+            const bank = document.getElementById('bank').value
+            const bankAccountNumber = document.getElementById('account-number').value
+            const amount = parseInt(document.getElementById('amount-to-add').value)
+            const pinNumber = parseInt(document.getElementById('pin-number').value)
+            const availableBalance = parseInt(document.getElementById('available-balance').innerText)
+            if(bankAccountNumber.length <11){
+                alert('Please Provide Valid Account Number')
+                return;
+            }
+            if(pinNumber != validPinNumber){
+                alert('Please Provide Valid Pin Number');
+                return;
+            }
+            const totalBalance = amount + availableBalance
+            document.getElementById('available-balance').innerText = totalBalance
+        })
