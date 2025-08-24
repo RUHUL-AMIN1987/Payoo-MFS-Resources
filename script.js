2cca9cd1b7089c72d69c1802611a68ba912026fe
Ruhul Amin <ruhulamin3052@gmail.com>
2025-08-25 00:00:56 +0600

    added all section

--- script.js
+++ script.js
@@ -4,13 +4,13 @@
 document.getElementById('loggingButton')
     .addEventListener('click', function(e){
         e.preventDefault()
-        const mobileNumber = '01714237145'
+        const mobileNumber = 17142371450
         const pinNumber = 7145
         const mobileNumberValue = document.getElementById('mobile-number').value
-        const mobileNumberValueConverted = mobileNumberValue
+        const mobileNumberValueConverted = parseInt(mobileNumberValue)
 
 
-        const pinNumberValue = document.getElementById('pin-number').value
+        const pinNumberValue = document.getElementById('pin-number').value 
         const pinNumberValueConverted = parseInt(pinNumberValue)
 
         if(mobileNumberValueConverted === mobileNumber && pinNumberValueConverted === pinNumber){
